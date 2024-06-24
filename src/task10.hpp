b4c1216786ca37dffe596c4d92ad9f6f087f84b1
SultanLenvu <slenvu@mail.ru>
2024-06-25 00:07:27 +0300

    task10

--- src/task10.hpp
+++ src/task10.hpp
@@ -11,36 +11,88 @@
 
 namespace Task10 {
 
-void solve() {
-  std::vector<std::unordered_set<int>> graph;
-  int N = 0, M = 0;
+enum class VertexColor {
+  White,
+  Grey,
+  Black,
+};
 
-  std::cin >> N >> M;
+void initGraph(std::vector<std::unordered_set<int>>& graph) {
+  int N = 0;
+
+  std::cin >> N;
   graph.resize(N);
 
-  int t1 = 0, t2 = 0;
-  for (int i = 0; i < M; i++) {
-    std::cin >> t1 >> t2;
-    graph[t1 - 1].insert(t2 - 1);
+  int t1 = 0;
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      std::cin >> t1;
+      if (t1 == 1) {
+        graph[i].insert(j);
+      }
+    }
   }
+}
 
-  std::vector<bool> visited;
-  visited[0] = true;
+void searchCycle(const std::vector<std::unordered_set<int>>& graph, int start,
+                 std::vector<VertexColor>& colors, std::vector<int>& cycle) {
   std::stack<int> need2visit;
-  need2visit.push(0);
+  need2visit.push(start);
+  colors[start] = VertexColor::Grey;
 
   while (!need2visit.empty()) {
     int current = need2visit.top();
     need2visit.pop();
+    cycle.push_back(current);
 
     for (int neighbor : graph[current]) {
-      if (visited[neighbor]) {
+      if ((cycle.size() >= 2) && (*(cycle.end() - 2) == neighbor)) {
+        continue;
+      }
+      if (colors[neighbor] == VertexColor::White) {
         need2visit.push(neighbor);
+        colors[neighbor] = VertexColor::Grey;
+      } else if (colors[neighbor] == VertexColor::Black) {
+        continue;
+      } else if (colors[neighbor] == VertexColor::Grey) {
+        cycle.push_back(neighbor);
+        return;
       }
     }
-
-    visited[current] = true;
   }
+  colors[start] = VertexColor::Black;
+}
+
+void printCycle(const std::vector<int>& cycle) {
+  if (cycle.size() == 0) {
+    std::cout << "NO";
+    return;
+  }
+
+  std::cout << "YES" << std::endl;
+  std::cout << cycle.size() << std::endl;
+  for (auto i : cycle) {
+    std::cout << i + 1 << " ";
+  }
+}
+
+void solve() {
+  std::vector<std::unordered_set<int>> graph;
+  initGraph(graph);
+
+  std::vector<VertexColor> colors(graph.size(), VertexColor::White);
+  std::vector<int> cycle;
+
+  for (int i = 0; i < graph.size(); ++i) {
+    if (colors[i] == VertexColor::White) {
+      searchCycle(graph, i, colors, cycle);
+    }
+    if (cycle.size() > 0) {
+      break;
+    }
+  }
+
+  printCycle(cycle);
 }
 
 };  // namespace Task10
